fa4e9894c0182bd8ca80887a91ddf3e7233fcca7
Colin Young <colin.young@inspire11.com>
2018-04-05 11:42:39 -0500

    Example app

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Linking } from 'react-native';
+import { AuthSession } from 'expo';
+
+const FB_APP_ID = '2059449127404706';
 
 export default class App extends React.Component {
+  state = {
+    initialUri: ''
+  };
+
+  _startAuthSession = async () => {
+    const redirectUrl = AuthSession.getRedirectUrl();
+    const result = await AuthSession.startAsync({
+      authUrl:
+        `https://www.facebook.com/v2.8/dialog/oauth?response_type=token` +
+        `&client_id=${FB_APP_ID}` +
+        `&redirect_uri=${encodeURIComponent(redirectUrl)}`,
+    });
+  }
+
+  async componentWillMount() {
+    const initialUri = await Linking.getInitialURL();
+    if (initialUri && initialUri.includes('expo-auth-session')) {
+      this.setState({ initialUri });
+    }
+  }
+
   render() {
+    const { initialUri } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Test AuthSession</Text>
+        <TouchableOpacity onPress={this._startAuthSession} style={styles.button}>
+          <Text style={styles.buttonText}>Test AuthSession right now</Text>
+        </TouchableOpacity>
+        {!!initialUri &&
+          <View>
+            <Text style={styles.error}>
+              {'Initial URI from AuthSession set, did bundle reload?'}
+            </Text>
+            <Text style={styles.infoText}>
+              {'Kill app to reset.'}
+            </Text>
+          </View>
+        }
       </View>
     );
   }
@@ -20,4 +56,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: 'blue',
+    margin: 20,
+    padding: 12,
+    borderRadius: 6,
+    borderWidth: 0
+  },
+  buttonText: {
+    color: '#fff'
+  },
+  error: {
+    color: 'red'
+  },
+  infoText: {
+    color: '#ccc',
+    fontSize: 12
+  }
 });
---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp597444c633054f3392ec03bc4b5307c4://192.168.16.47:19000";
+
+}
